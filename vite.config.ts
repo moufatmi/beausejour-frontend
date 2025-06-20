36d3d1ad9c3356791507a864fc311c76aa89827f
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-20 17:50:02 +0100

    'update'

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/beausejour-frontend/',
-=======
-  base: "/beausejour-frontend/",
->>>>>>> 44c14e0d582a5ab5b7037c30bfe5770ff1c66704
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
