87efb2f79f549b6f732b451e65ef3b11cc0397e8
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-22 02:16:02 +0100

    Updates

--- assets/FlightResults-CzXk6F0A.js
+++ assets/FlightResults-CzXk6F0A.js
@@ -1,4 +1,4 @@
-import{c as r,j as e,P as m,M as c}from"./index-BV9xKHj1.js";/**
+import{c as r,j as e,P as m,M as c}from"./index-Cyiw76VR.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
