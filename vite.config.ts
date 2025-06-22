efefc382266a56eb8b6f1ad9ded951c95101a71a
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-22 23:15:39 +0100

    'update'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://ticket.beausejourvoyage.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
