748f3714bb14c6b5190bfcefdf1f261ad5633bf7
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-24 19:06:56 +0100

    Updates

--- assets/FlightResults-DFoicIey.js
+++ assets/FlightResults-DFoicIey.js
@@ -1,4 +1,4 @@
-import{c as r,j as e,P as m,M as c}from"./index-BDyINdbM.js";/**
+import{c as r,j as e,P as m,M as c}from"./index-BW2c_Ms6.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
