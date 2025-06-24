7006d985f7d2a42f99ea939b44e38cbfc07b7117
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-24 18:59:29 +0100

    Updates

--- assets/FlightResults-DWLkshLn.js
+++ assets/FlightResults-DWLkshLn.js
@@ -1,4 +1,4 @@
-import{c as r,j as e,P as m,M as c}from"./index-BU0AlUNm.js";/**
+import{c as r,j as e,P as m,M as c}from"./index-BTwuZh89.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
