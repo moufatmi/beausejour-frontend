5cc56563dfbdee512d9ece11b3cb477659642177
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-06-24 12:30:51 +0100

    Updates

--- assets/FlightResults-DHmJDmf5.js
+++ assets/FlightResults-DHmJDmf5.js
@@ -1,4 +1,4 @@
-import{c as r,j as e,P as m,M as c}from"./index-BuvG1OBu.js";/**
+import{c as r,j as e,P as m,M as c}from"./index-nHftpNW1.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
